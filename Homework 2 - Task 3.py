51e3647b6bbb9e1d86d6c15e4e335e78a3ea0b1d
Giorgi Sandroshvili <63731060+gsandroshvili@users.noreply.github.com>
2022-04-11 22:16:13 +0400

    Add files via upload
    Homework 2 Done

---
+++ Homework 2 - Task 3.py
@@ -0,0 +1,24 @@
+import boto3
+from argparse import ArgumentParser
+
+def parser():
+    parser = ArgumentParser()
+    parser.add_argument('-n', '--name', type=str, required=True)        
+    parsed_arguments = parser.parse_args()
+    return parsed_arguments
+
+def exists(s3, name):
+    response = s3.head_bucket(Bucket=name)
+    return response["ResponseMetadata"]["HTTPStatusCode"] == 200
+
+def delete():
+    parsed_arguments = parser()
+    name = parsed_arguments.name
+    s3 = boto3.client('s3')
+    if exists(s3, name):
+        s3.delete_bucket(Bucket=name)
+    else:
+        print(f"Bucket does not exist")
+
+if __name__ == '__main__':
+    delete()
